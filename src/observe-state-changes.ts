053b00a805c33ec90edc6427b4428cfc5a9f8c23
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-20 07:08:48 +0000

    refactor(observe-state-changes): improve readability

--- src/observe-state-changes.ts
+++ src/observe-state-changes.ts
@@ -1,11 +1,6 @@
 import { Observable, Subscriber, fromEvent, merge } from 'rxjs'
 import { map } from 'rxjs/operators'
 
-const pushStateHooks = new Set<Subscriber<void>>()
-const replaceStateHooks = new Set<Subscriber<void>>()
-let pushStateHookRegistered = false
-let replaceStateHookRegistered = false
-
 export function observeStateChanges(): Observable<void> {
   return merge(
     observePushState()
@@ -17,40 +12,52 @@ export function observeStateChanges(): Observable<void> {
   )
 }
 
+const pushStateHooks = new Set<Subscriber<void>>()
+let pushStateHookRegistered = false
+
 function observePushState(): Observable<void> {
   return new Observable(observer => {
-    if (!pushStateHookRegistered) {
-      registerPushStateHook()
-    }
+    ensurePushStateHook()
+
     pushStateHooks.add(observer)
     return () => pushStateHooks.delete(observer)
   })
+
+  function ensurePushStateHook(): void {
+    if (pushStateHookRegistered) return
+
+    const pushState = history.pushState
+    // 该猴子补丁不可复原, 因为在打上补丁后可能有其他代码也选择劫持`history.pushState`.
+    history.pushState = function (...args) {
+      Reflect.apply(pushState, this, args)
+      pushStateHooks.forEach(observer => observer.next())
+    }
+
+    pushStateHookRegistered = true
+  }
 }
+
+const replaceStateHooks = new Set<Subscriber<void>>()
+let replaceStateHookRegistered = false
 
 function observeReplaceState(): Observable<void> {
   return new Observable(observer => {
-    if (!replaceStateHookRegistered) {
-      registerReplaceStateHook()
-    }
+    ensureReplaceStateHook()
+
     replaceStateHooks.add(observer)
     return () => replaceStateHooks.delete(observer)
   })
-}
 
-function registerPushStateHook() {
-  const pushState = history.pushState
-  history.pushState = function (...args) {
-    Reflect.apply(pushState, this, args)
-    pushStateHooks.forEach(observer => observer.next())
-  }
-  pushStateHookRegistered = true
-}
+  function ensureReplaceStateHook(): void {
+    if (replaceStateHookRegistered) return
 
-function registerReplaceStateHook() {
-  const replaceState = history.replaceState
-  history.replaceState = function (...args) {
-    Reflect.apply(replaceState, this, args)
-    replaceStateHooks.forEach(observer => observer.next())
+    const replaceState = history.replaceState
+    // 该猴子补丁不可复原, 因为在打上补丁后可能有其他代码也选择劫持`history.replaceState`.
+    history.replaceState = function (...args) {
+      Reflect.apply(replaceState, this, args)
+      replaceStateHooks.forEach(observer => observer.next())
+    }
+
+    replaceStateHookRegistered = true
   }
-  replaceStateHookRegistered = true
 }
